54b8479ddb8ef3e29a2b88dcc4ecaae58a8497e6
Vlad <sotinvlad@yandex.ru>
2022-07-23 23:36:28 +0300

    Убран warning

--- src/components/GoodItem/GoodItem.jsx
+++ src/components/GoodItem/GoodItem.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 import styles from './GoodItem.module.scss';
 
-const GoodItem = ({ itemTitle, itemDescription, pictureLink, itemPrice }) => {
+const GoodItem = ({
+    id,
+    itemTitle,
+    itemDescription,
+    pictureLink,
+    itemPrice,
+}) => {
     return <div className={styles.GoodItem}>GoodItem</div>;
 };
 export default GoodItem;
