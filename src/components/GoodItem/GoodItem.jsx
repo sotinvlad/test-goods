efa59e83cd79bbf4e3b1a153ff8d879087aaf65b
Vlad <sotinvlad@yandex.ru>
2022-07-24 00:35:34 +0300

    Адаптивная верстка товаров

--- src/components/GoodItem/GoodItem.jsx
+++ src/components/GoodItem/GoodItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import styles from './GoodItem.module.scss';
+import defaultImage from './../../assets/basicItemPicture.png';
 
 const GoodItem = ({
     id,
@@ -9,6 +10,13 @@ const GoodItem = ({
     pictureLink,
     itemPrice,
 }) => {
-    return <div className={styles.GoodItem}>GoodItem</div>;
+    return (
+        <div className={styles.GoodItem}>
+            <img src={pictureLink || defaultImage} alt='itemPicture' />
+            <div className={styles.ItemTitle}>{itemTitle}</div>
+            <div className={styles.ItemDescription}>{itemDescription}</div>
+            <div className={styles.ItemPrice}>{itemPrice} руб.</div>
+        </div>
+    );
 };
 export default GoodItem;
