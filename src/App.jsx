9fd0d9223e81fc75a952e47e51fac1f2fabd696c
Vlad <sotinvlad@yandex.ru>
2022-07-24 10:43:50 +0300

    Удаление товаров

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import styles from './App.module.scss';
 import Form from './components/Form/Form';
 import GoodsList from './components/GoodsList/GoodsList';
 import Header from './components/Header/Header';
+import deleteItemById from './helpers/deleteItemById';
 
 function App() {
     const [items, setItems] = useState(goods);
-    useEffect(() => {
-        setItems(goods);
-    }, []);
+    const deleteItem = (id) => {
+        const newArrayOfItems = deleteItemById(id, items);
+        setItems(newArrayOfItems);
+    };
     return (
         <div className={styles.App}>
             <Header typeOfSorting={'default'} />
             <div className={styles.MainContent}>
                 <Form />
-                <GoodsList items={items} />
+                <GoodsList items={items} deleteItem={deleteItem} />
             </div>
         </div>
     );
--- src/components/GoodItem/GoodItem.jsx
+++ src/components/GoodItem/GoodItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import styles from './GoodItem.module.scss';
 import defaultImage from './../../assets/basicItemPicture.png';
+import deleteBucket from './../../assets/deleteBucket.png';
 
 const GoodItem = ({
     id,
@@ -9,13 +10,22 @@ const GoodItem = ({
     itemDescription,
     pictureLink,
     itemPrice,
+    deleteItem,
 }) => {
+    const onDeleteButtonClick = () => {
+        deleteItem(id);
+    };
     return (
         <div className={styles.GoodItem}>
-            <img src={pictureLink || defaultImage} alt='itemPicture' />
+            <img src={pictureLink || defaultImage} alt='Изображение товара' />
             <div className={styles.ItemTitle}>{itemTitle}</div>
             <div className={styles.ItemDescription}>{itemDescription}</div>
             <div className={styles.ItemPrice}>{itemPrice} руб.</div>
+            <div
+                className={styles.DeleteButton}
+                onClick={() => onDeleteButtonClick()}>
+                <img src={deleteBucket} alt='Кнопка удаления товара' />
+            </div>
         </div>
     );
 };
